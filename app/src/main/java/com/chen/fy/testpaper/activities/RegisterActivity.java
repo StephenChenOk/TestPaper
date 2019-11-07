13155b543ed3f8f1b550c4d892d1a7a1d2b31912
1670617529@qq.com <1670617529@qq.com>
2019-11-07 10:16:34 +0800

    更改注册界面

--- app/src/main/java/com/chen/fy/testpaper/activities/RegisterActivity.java
+++ app/src/main/java/com/chen/fy/testpaper/activities/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chen.fy.testpaper.R;
@@ -19,7 +21,6 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private EditText etUsername;
     private EditText etPwd;
     private EditText etPwd2;
-    private Button btnRegister;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,40 +39,52 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         etUsername = findViewById(R.id.et_username);
         etPwd = findViewById(R.id.et_pwd);
         etPwd2 = findViewById(R.id.et_pwd_again);
-        btnRegister = findViewById(R.id.btn_register);
+        Button btnRegister = findViewById(R.id.btn_register);
+        ImageView ivReturn = findViewById(R.id.iv_return);
+        TextView tvReturn = findViewById(R.id.tv_return);
 
         btnRegister.setOnClickListener(this);
+        ivReturn.setOnClickListener(this);
+        tvReturn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_register:
-
-                String username = etUsername.getText().toString();
-                //用户名已填入且没有被注册过时
-                if(LoginRegisterUtils.userAvailable(username)){
-                    String password1 = etPwd.getText().toString();
-                    String password2 = etPwd2.getText().toString();
-                    //判断两次密码是否相等
-                    if(LoginRegisterUtils.passwordSame(password1,password2)){
-                        //获取系统随机生成的值(盐值),用于加密
-                        String pwSalt = UUID.randomUUID().toString().substring(0, 5);
-                        //用输入的密码+盐值进行MD5加密
-                        String pwHash = LoginRegisterUtils.getMD5(password1 + pwSalt);
-                        //存入数据库
-                        User visitor = new User();
-                        visitor.setUsername(username);
-                        visitor.setPwSalt(pwSalt);
-                        visitor.setPwHash(pwHash);
-                        visitor.save();
-                        //注册成功后跳到登入界面
-                        Toast.makeText(RegisterActivity.this,"注册成功,可以进行登入操作",Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                }
-
+                goRegester();
                 break;
+            case R.id.iv_return:
+                finish();
+                break;
+            case R.id.tv_return:
+                finish();
+                break;
+        }
+    }
+
+    private void goRegester() {
+        String username = etUsername.getText().toString();
+        //用户名已填入且没有被注册过时
+        if(LoginRegisterUtils.userAvailable(username)){
+            String password1 = etPwd.getText().toString();
+            String password2 = etPwd2.getText().toString();
+            //判断两次密码是否相等
+            if(LoginRegisterUtils.passwordSame(password1,password2)){
+                //获取系统随机生成的值(盐值),用于加密
+                String pwSalt = UUID.randomUUID().toString().substring(0, 5);
+                //用输入的密码+盐值进行MD5加密
+                String pwHash = LoginRegisterUtils.getMD5(password1 + pwSalt);
+                //存入数据库
+                User visitor = new User();
+                visitor.setUsername(username);
+                visitor.setPwSalt(pwSalt);
+                visitor.setPwHash(pwHash);
+                visitor.save();
+                //注册成功后跳到登入界面
+                Toast.makeText(RegisterActivity.this,"注册成功,可以进行登入操作",Toast.LENGTH_SHORT).show();
+                finish();
+            }
         }
     }
 }
